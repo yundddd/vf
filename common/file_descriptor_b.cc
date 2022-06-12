147d6ca7658aa10f9588d73947a0e339c7c65050
Tim Tianyang Chen <soapcn@gmail.com>
2022-06-12 23:46:33 +0000

    wip

--- common/file_descriptor_b.cc
+++ common/file_descriptor_b.cc
@@ -1,8 +1,11 @@
 
+#include "arch/syscall.hh"
 
-#include "common/file_descriptor.hh"
-using namespace vt;
-int main(){
-    common::FileDescriptor f("/tmp/a", 1 | 64);
-    return 0;
+int main(int argc, char* argv[]) {
+  write(1, "asd\n", 5);
+  int fd = sys_open("/tmp/aaa", 1 | 64, 0400 | 0200 | 0100);
+  write(fd, "asd\n", 5);
+  // std::cout << f.handle() << std::endl;
+  //::write(__res, "abc", 3);
+  return 0;
 }
