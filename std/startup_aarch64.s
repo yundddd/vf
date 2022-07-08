654fb6cf705ef17259485547c9b79ee3551f967d
Tim Tianyang Chen <soapcn@gmail.com>
2022-07-07 23:45:02 -0700

    fix aarch64

--- std/startup_aarch64.s
+++ std/startup_aarch64.s
@@ -9,7 +9,6 @@ add x2, x2, x1     /*           + argv */
 and sp, x1, -16    /* sp must be 16-byte aligned in the callee */
 adrp x3, _environ  /* save envp to global */
 str x2, [x3, :lo12:_environ]
-bl main     /* main() returns the status code, we'll exit with it. */
-bl main      
+bl main     /* main() returns the status code, we'll exit with it. */    
 mov x8, 93   
 svc #0
