c8c1fb67cedc0e2ba16b7df2866e485328ef8eed
Tim Tianyang Chen <soapcn@gmail.com>
2023-08-10 15:06:10 -0700

    remove-volume

--- setup_dev_imagies.sh
+++ setup_dev_imagies.sh
@@ -1,15 +1,9 @@
 #/bin/bash
 
 # Run this script once. If the dockerfile is modified, please remove
-# all created images. containers and volumes first.
+# all created images containers first before running it again.
 
-# switch driver to build a image for both arm and x86
-docker buildx create --use
 # biuld for both arm and x86 
-docker buildx build --platform linux/amd64,linux/arm64 . -t ubuntu-multi-arch
-# load from cache and tag it: 
 docker buildx build --load --platform linux/amd64 -t ubuntu-amd64-img .
 docker buildx build --load --platform linux/arm64 -t ubuntu-arm64-img .
 
-# create a volume that persists repo data
-docker volume create RepoVolume
