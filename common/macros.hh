bd3f4dcd8ef76c2eb5f7647bf01139e93d607cfd
Tim Tianyang Chen <soapcn@gmail.com>
2023-09-07 14:01:09 -0700

    better str literal macro

--- common/macros.hh
+++ common/macros.hh
@@ -24,12 +24,17 @@
   MAKE_COPYABLE(class_name);                  \
   MAKE_MOVABLE(class_name);
 
-// Viruses can be inserted into different places and run, therefore they cannot
-// reference strings stored in .rodata. This can be worked around by injecting
-// .rodata together with .text at the expense of a larger than desired virus.
-// Some syscall however requires initialized string to work. To support this, we
-// have provided the STR_LITERAL macro to put chars in the .text section. For
-// small strings (less than 8 chars including the null terminator), users may do
+// Problem:
+// Some syscall requires initialized string to work but our viruses must be
+// self-contained. Therefore they cannot reference strings stored in .rodata.
+// This can be worked around by merging virus's .rodata together with .text at
+// the expense of a larger than desired virus. But this only works for infection
+// methods that guarentee page alignment for arm, making the virus code
+// non-portable.
+
+// Solutions:
+// For small strings (less than 8 chars including the null terminator), users
+// may do:
 //          char str[8] = {'m', 'y', '_', 's', 't', 'r', 0};
 // which generates code to initialize the string on stack. Anything longer
 // _might_ be put in .rodata. Users should always confirm that the virus has no
@@ -41,60 +46,51 @@
 //          hello[0] = 'h'; hello[1] = 'e'; hello[2] = 'l'; ...
 // This is very error prone and if the string is long, can take up lot of code
 // space. Please consider the STR_LITERAL macro instead.
-
-// The following macro uses a trick common in buffer overflow that loads the
-// string literal address by a function call. It uses an extra 24 bytes
-// (aarch64) or 10 bytes (x86-64 variable length inst to the win!) of
-// instructions for loading, which is not too bad for long strings. Wrapping
-// string literals with this macro will ensure they show up in .text instead of
-// in .rodata, required by most parasites. For example:
 //
+// The following macro uses an extra 8 bytes (aarch64) or 9 bytes (x86-64) of
+// instructions for loading string address that resides in the text segement,
+// which is not too bad for long strings.
+// example:
+//
+//   // note the extra escape for both % and back slash.
 //   const char* str =
-//      STR_LITERAL("this binary is infected\\n");
-//   write(1, str, strlen(str));
+//      STR_LITERAL("this binary %%s is infected\\n");
+//   vt::printf(str, argv[0]);
 //
 // If it's guarenteed that the virus is relocated to a page aligned address,
 // users can consider merging .text and .rodata without using this macro. Note
 // that we do not align the next instruction on 4 byte boundary on x86, but we
 // do that for aarch64 since arm instruction must be aligned.
 #if defined(__x86_64__)
-#define STR_LITERAL(str, literal) \
-  [] {                            \
-    const char* str;              \
-    asm("jmp 1f\n"                \
-        "2:\n"                    \
-        "pop %0\n"                \
-        "jmp 3f\n"                \
-        "1:\n"                    \
-        "call 2b\n"               \
-        ".asciz \"" literal       \
-        "\"\n"                    \
-        "3:\n"                    \
-        : "=r"(str)               \
-        :);                       \
-    return str;                   \
+#define STR_LITERAL(literal)   \
+  [] {                         \
+    const char* str;           \
+    asm("lea 1f(%%rip), %0 \n" \
+        "jmp 3f\n"             \
+        "1:\n"                 \
+        ".asciz \"" literal    \
+        "\"\n"                 \
+        "3:\n"                 \
+        : "=r"(str)            \
+        :);                    \
+    return str;                \
   }()
 
 #elif defined(__aarch64__)
 // Aarch64 must run instructions aligned to 4-byte addresses and therefore we
 // pad the string transparently for users.
-#define STR_LITERAL(literal)          \
-  [] {                                \
-    const char* str;                  \
-    asm("stp x29, x30, [sp, #-16]!\n" \
-        "b 1f\n"                      \
-        "2:\n"                        \
-        "mov %0, x30\n"               \
-        "ldp x29, x30, [sp], #16\n"   \
-        "b 3f\n"                      \
-        "1:\n"                        \
-        "bl 2b\n"                     \
-        ".asciz \"" literal           \
-        "\"\n"                        \
-        ".align 4\n"                  \
-        "3:\n"                        \
-        : "=r"(str)                   \
-        :);                           \
-    return str;                       \
+#define STR_LITERAL(literal) \
+  [] {                       \
+    const char* str;         \
+    asm("adr %0, 1f\n"       \
+        "b 2f\n"             \
+        "1:\n"               \
+        ".asciz \"" literal  \
+        "\"\n"               \
+        ".align 4\n"         \
+        "2:\n"               \
+        : "=r"(str)          \
+        :);                  \
+    return str;              \
   }()
 #endif
