fd9e78a43a54d30dd11334d66b2beb6daa92e1de
Tim Tianyang Chen <soapcn@gmail.com>
2024-06-19 15:48:49 -0700

    [victim] make victim long running
    Add an endless loop so that the victim binary runs forever. This
    makes inspecting runtime memory mapping of the victim easier.

--- infector/victim.cc
+++ infector/victim.cc
@@ -2,5 +2,9 @@
 
 int main() {
   std::cout << "victim binary is running!" << std::endl;
+  // Keeps the victim long running so we can inspect various
+  // runtime attributes of this program after being injected
+  // with a virus.
+  while(1);
   return 0;
 }
