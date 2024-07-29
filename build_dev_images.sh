f4125664e2724f6f24b9574a7dafd2ca3f96de1a
Tim Tianyang Chen <soapcn@gmail.com>
2024-07-28 21:19:43 -0700

    [docker] improve dev container scripts
    Allow users to specify arch name to selectively build and run
    containers.

--- build_dev_images.sh
+++ build_dev_images.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+# This script can build development containers for both x86 and arm. Usage:
+
+# // Build arm container only
+# ./build_dev_images.sh arm
+
+# // Build x86 container only
+# ./build_dev_images.sh x86
+
+# // Build both
+# ./build_dev_images.sh
+
+build_x86() {
+	docker buildx build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --build-arg USERNAME="$(whoami)" --load --platform linux/amd64 -t ubuntu-x86-img .
+}
+
+build_arm() {
+	docker buildx build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --build-arg USERNAME="$(whoami)" --load --platform linux/arm64 -t ubuntu-aarch64-img .
+}
+
+if [ $# -gt 1 ]; then
+	echo "Error: Too many arguments provided"
+	exit 1
+fi
+
+if [ -z "$1" ]; then
+	echo "Building docker images for both x86 and arm"
+	build_arm
+	build_x86
+	exit 0
+fi
+
 # build dev images for both arm and x86.
-docker buildx build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --build-arg USERNAME="$(whoami)" --load --platform linux/amd64 -t ubuntu-x86-img .
-docker buildx build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --build-arg USERNAME="$(whoami)" --load --platform linux/arm64 -t ubuntu-aarch64-img .
+if [ "$1" == "arm" ]; then
+	build_arm
+elif [ "$1" == "x86" ]; then
+	build_x86
+else
+	echo "Unknown architecture " $1
+fi
--- run_dev_containers.sh
+++ run_dev_containers.sh
@@ -10,19 +10,42 @@ docker ps -aq --filter "name=${AARCH64_CONTAINER}" | xargs -r docker rm -f
 USERNAME=$(whoami)
 USERHOME=/home/$USERNAME
 
-# start the container in interactive, detached mode and mount a volume for repo data.
-# The container will be removed when exited and all persistent data should be in the
-# repo directory and shared with the host.
-docker run -h x86 -tid \
-	--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-	-v "$(pwd)":"${USERHOME}"/vf:z \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	--name "${X86_CONTAINER}" ubuntu-x86-img
-docker run -h aarch64 -tid \
-	--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-	-v "$(pwd)":"${USERHOME}"/vf:z \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	--name "${AARCH64_CONTAINER}" ubuntu-aarch64-img
+run_x86() {
+	docker run -h x86 -tid \
+		--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+		-v "$(pwd)":"${USERHOME}"/vf:z \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		--name "${X86_CONTAINER}" ubuntu-x86-img
+}
 
-docker container prune -f
-docker image prune -f -a
+run_arm() {
+	docker run -h aarch64 -tid \
+		--cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+		-v "$(pwd)":"${USERHOME}"/vf:z \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		--name "${AARCH64_CONTAINER}" ubuntu-aarch64-img
+}
+
+if [ $# -gt 1 ]; then
+	echo "Error: Too many arguments provided"
+	exit 1
+fi
+
+if [ -z "$1" ]; then
+	echo "Building docker images for both x86 and arm"
+	run_arm
+	run_x86
+	exit 0
+fi
+
+# build dev images for both arm and x86.
+if [ "$1" == "arm" ]; then
+	run_arm
+elif [ "$1" == "x86" ]; then
+	run_x86
+else
+	echo "Unknown architecture " $1
+fi
+
+docker container prune -f --filter "label!=$AARCH64_CONTAINER" --filter "label!=$X86_CONTAINER"
+docker image prune -f -a --filter "label!=$AARCH64_CONTAINER-img" --filter "label!=$X86_CONTAINER-img"
